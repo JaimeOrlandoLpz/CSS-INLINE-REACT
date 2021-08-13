9363c0f607a9ac7376254d870bf83b4439382aea
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2021-08-13 00:00:41 -0500

    Initial

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+const pinkStyle = {
+  color: "pink",
+  fontSize: "20px",
+  border: "1px solid black"
+};
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+// We can actually modify this here
+pinkStyle.color = "magenta";
+
+ReactDOM.render(
+  <h1 style={pinkStyle}>Hello World!</h1>,
+  document.getElementById("root")
+);
